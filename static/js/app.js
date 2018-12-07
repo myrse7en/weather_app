2e7b01bd3bbbd4a6f223eddf1082273603a16e59
Mayur Bhatia <23645213+myrse7en@users.noreply.github.com>
2018-12-07 11:32:49 -0500

    API call and display working correctly

--- static/js/app.js
+++ static/js/app.js
@@ -7,3 +7,41 @@ const API_KEY = config.getKey();
 $.get('../../components/header.html', function(response) {
   $('#nav').html(response)
 });
+
+
+// don't show cards until form is submitted
+$("#weather_info").css('display', 'none');
+
+// convert kelvin to fahrenjeit which takes in a single number
+function convertKelvin(k) {
+  //  F = 9/5 (K - 273) + 32
+  let f =  (9/5) * (k - 273) + 32;
+  return f.toFixed(2);
+}
+
+// handle form submission by using jQuery to listen for event
+$("#search_weather").submit(event => {
+  event.preventDefault();
+
+  // console.log(event);
+
+  let city = $("#city").val();
+  console.log(city);
+  // create url variable to store api location
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`;
+
+  // console.log(url);
+
+  // use jQuery .get to call API
+  $.get(url, function(response) {
+    console.log(response);
+
+    // make cards appear
+    $("#weather_info").css('display', 'block');
+
+    $("#high").html(`${convertKelvin(response.main.temp_max)}&deg;`);
+    $("#low").html(`${convertKelvin(response.main.temp_min)}&deg;`);
+    $("#forecast").text(response.weather[0].description);
+    $("#humidity").html(`${response.main.humidity}%`);
+  });
+});
