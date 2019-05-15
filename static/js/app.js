64b776d2d11c0bf31f599acdbce14697aedc0330
Mayur Bhatia <23645213+myrse7en@users.noreply.github.com>
2019-05-15 18:21:42 -0400

    Minor fix

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,7 @@ const API_KEY = config.getKey();
 
 // request header and put into nav id
 $.get('../../components/header.html', function(response) {
-  $('#nav').html(response)
+  $('#nav').html(response);
 });
 
 
